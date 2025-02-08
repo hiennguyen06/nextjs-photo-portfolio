5446b21c38a2ddd44585118b090f299f28442cd4
Hien Nguyen <hiennguyen1529@gmail.com>
2025-02-09 00:10:12 +1100

    add cloudingary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { getImagesFromCloudinary } from "@/lib/cloudinaryConfig";
+import { getImagesFromCloudinary } from "@/lib/cloudinary";
 import ImageModal from "@/app/components/ImageModal";
 
 export const revalidate = 3600; // Revalidate every hour
